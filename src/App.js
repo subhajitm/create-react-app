5751c970617f500026291e77e4cb731f00b40947
Subhajit Mandal <sumand@microsoft.com>
2018-02-06 23:09:40 -0800

    dynamic manipulation of state

--- src/App.js
+++ src/App.js
@@ -3,11 +3,37 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'max', age: 23 },
+      { name: 'manu', age: 12 }
+    ],
+    otherState: 'some other state'
+  }
+
+  switchHandler = (newName) => {
+    // console.log('was clicked');
+    // this.state.persons[0].name = 'subhajit';
+    this.setState({persons:[
+      { name: newName, age: 23 },
+      { name: 'manu', age: 12 }
+    ]})
+  }
   render() {
     return (
       <div className="App">
         <h1>React app</h1>
-        <Person />
+        <button onClick={() => this.switchHandler('subho')}>switch</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age} />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+          click={this.switchHandler.bind(this, 'Raj')} />
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}>Sending Childern</Person>
       </div>
     );
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
-const person = () => {
-    return <p>I'm a Person</p>
+const person = (arg) => {
+    return (
+        <div>
+            <p onClick={arg.click}>I'm {arg.name} and age {arg.age}</p>
+            <p>{arg.children}</p>
+        </div>
+    )
 }
 
 export default person;
